03df6c286637cd881d27b1293b6954655daff842
dencel007 <dencelkbabu007@gmail.com>
2019-04-02 19:39:02 +0530

    Revert "msm: kgsl: Remove POPP"
    This reverts commit 2ec552b82e2dff32dec706b8efdbb440d221c880.

--- drivers/gpu/msm/kgsl_pwrctrl.h
+++ drivers/gpu/msm/kgsl_pwrctrl.h
@@ -115,6 +115,7 @@ struct kgsl_regulator {
  * @active_pwrlevel - The currently active power level
  * @previous_pwrlevel - The power level before transition
  * @thermal_pwrlevel - maximum powerlevel constraint from thermal
+ * @default_pwrlevel - device wake up power level
  * @restrict_pwrlevel - maximum power level jump to restrict
  * @max_pwrlevel - maximum allowable powerlevel per the user
  * @min_pwrlevel - minimum allowable powerlevel per the user
@@ -168,6 +169,7 @@ struct kgsl_pwrctrl {
 	unsigned int active_pwrlevel;
 	unsigned int previous_pwrlevel;
 	unsigned int thermal_pwrlevel;
+	unsigned int default_pwrlevel;
 	unsigned int restrict_pwrlevel;
 	unsigned int wakeup_maxpwrlevel;
 	unsigned int max_pwrlevel;
